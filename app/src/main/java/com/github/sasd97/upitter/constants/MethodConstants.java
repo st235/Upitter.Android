3da52b38ad08e3d01416c3fbdc73f979324f25e3
Alexander Dadukin <st235@yandex.ru>
2016-06-14 17:43:58 +0300

    Фиксы адресов сервера

--- app/src/main/java/com/github/sasd97/upitter/constants/MethodConstants.java
+++ app/src/main/java/com/github/sasd97/upitter/constants/MethodConstants.java
@@ -5,6 +5,9 @@ package com.github.sasd97.upitter.constants;
  */
 public class MethodConstants {
 
-    public static final String BASE_API_URL = "http://192.168.1.11:9000/";
+    public static final String BASE_API_URL = "http://192.168.1.105:9000/";
     public static final String FILE_SERVER_API_URL = "http://127.0.0.1:3000/";
+
+    public static final String BASE_SUB_API_URL = "http://192.168.1.11:9000/";
+    public static final String FILE_SUB_SERVER_API_URL = "http://192.168.1.11:3000/";
 }
