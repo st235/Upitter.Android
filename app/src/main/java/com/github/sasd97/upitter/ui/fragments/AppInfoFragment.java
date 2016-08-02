dfc0f242748237c86241b936bc0361721732340b
Alexadner Dadukin <st235@yandex.ru>
2016-08-03 00:32:14 +0300

    Переименован (fragments): AboutApp -> AppInfo

--- app/src/main/java/com/github/sasd97/upitter/ui/fragments/AppInfoFragment.java
+++ app/src/main/java/com/github/sasd97/upitter/ui/fragments/AppInfoFragment.java
@@ -10,19 +10,19 @@ import com.github.sasd97.upitter.ui.base.BaseFragment;
 /**
  * Created by alexander on 08.07.16.
  */
-public class AboutAppFragment extends BaseFragment {
+public class AppInfoFragment extends BaseFragment {
 
-    public AboutAppFragment() {
-        super(R.layout.about_app_fragment);
-    }
-
-    @Override
-    protected void setupViews() {
-
+    public AppInfoFragment() {
+        super(R.layout.fragment_app_info);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    protected void setupViews() {
+
     }
 }
