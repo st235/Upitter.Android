68024e7bb943f43432887004ed5da68ca9b360c0
Alexadner Dadukin <st235@yandex.ru>
2016-08-03 00:54:28 +0300

    Переименован (fragments): Location ->LocationSelection

---
+++ app/src/main/java/com/github/sasd97/upitter/ui/fragments/CompanyLocationMapSelectionFragment.java
@@ -0,0 +1,20 @@
+package com.github.sasd97.upitter.ui.fragments;
+
+import com.github.sasd97.upitter.R;
+import com.github.sasd97.upitter.ui.base.BaseFragment;
+
+/**
+ * Created by alexander on 01.08.16.
+ */
+
+public class CompanyLocationMapSelectionFragment extends BaseFragment {
+
+    public CompanyLocationMapSelectionFragment() {
+        super(R.layout.tape_fragment);
+    }
+
+    @Override
+    protected void setupViews() {
+
+    }
+}
