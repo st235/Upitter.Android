a5bf188556fbee6cbb905f1741ebd3e4eb2a1b8e
Alexadner Dadukin <st235@yandex.ru>
2016-08-03 00:55:47 +0300

    Переименован (fragments): Notifications ->Notification

---
+++ app/src/main/java/com/github/sasd97/upitter/ui/fragments/NotificationFragment.java
@@ -0,0 +1,19 @@
+package com.github.sasd97.upitter.ui.fragments;
+
+import com.github.sasd97.upitter.R;
+import com.github.sasd97.upitter.ui.base.BaseFragment;
+
+/**
+ * Created by alexander on 08.07.16.
+ */
+public class NotificationFragment extends BaseFragment {
+
+    public NotificationFragment() {
+        super(R.layout.fragment_notification);
+    }
+
+    @Override
+    protected void setupViews() {
+
+    }
+}
