af830207b2dfdd906c85a5a0e3eb1648e440f1d4
Alexadner Dadukin <st235@yandex.ru>
2016-08-03 00:30:48 +0300

    Переименован: SocialIcons -> SocialIconsInstallation

--- app/src/main/java/com/github/sasd97/upitter/ui/results/SocialIconInstallationResult.java
+++ app/src/main/java/com/github/sasd97/upitter/ui/results/SocialIconInstallationResult.java
@@ -1,15 +1,20 @@
 package com.github.sasd97.upitter.ui.results;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.sasd97.upitter.R;
+import com.github.sasd97.upitter.ui.base.BaseActivity;
 
-public class SocialIconsActivity extends AppCompatActivity {
+public class SocialIconInstallationResult extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.social_icons_activity);
+        setContentView(R.layout.activity_social_icons);
+    }
+
+    @Override
+    protected void setupViews() {
+
     }
 }
